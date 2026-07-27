f8664ccbc5078c3f02793d594cb12d8e99287317
Minh Lê <22520865@gm.uit.edu.vn>
2026-07-27 22:09:25 +0700

    delete before migrate db

--- REST/productDB.js
+++ REST/productDB.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const start = async () => {
     try {
         await connectDB(process.env.MONGODB_URL);
+        await Product.deleteMany();
         await Product.create(ProductJson);
         console.log("success");
     } catch (error) {
