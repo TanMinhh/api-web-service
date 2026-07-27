b50c8a12ac91b85092c08a1b0ff77ee631b921d6
Minh Lê <22520865@gm.uit.edu.vn>
2026-07-27 22:10:46 +0700

    query, filter, sort, select, pagination

--- REST/controllers/products.js
+++ REST/controllers/products.js
@@ -1,9 +1,108 @@
+const Products = require("../models/products");
+
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "Products" });
+    const myData = await Products.find({});
+    res.status(200).json({ myData });
 };
 
 const getAllProductsTest = async (req, res) => {
-    res.status(200).json({ msg: "Products Test Success" });
+    const myBanana = await Products.find({ name: "banana" });
+    res.status(200).json({ myBanana });
 };
 
-module.exports = { getAllProducts, getAllProductsTest };
+const getQueryProducts = async (req, res) => {
+    const myData = await Products.find(req.query);
+    res.status(200).json({ myData });
+};
+
+const getQueryProductsWithCompanyFilter = async (req, res) => {
+    const { company } = req.query;
+    const queryObj = {};
+    if (company) {
+        queryObj.company = company;
+    }
+
+    const myData = await Products.find(queryObj);
+    res.status(200).json({ myData });
+};
+
+const getQueryProductsWithFilters = async (req, res) => {
+    const { company, name } = req.query;
+    const queryObj = {};
+    if (company) {
+        queryObj.company = company;
+    }
+    if (name) {
+        queryObj.name = { $regex: name, $options: "i" };
+        //Full text search functionality - $regex with $options
+    }
+
+    const myData = await Products.find(queryObj);
+    res.status(200).json({ myData });
+};
+
+const getSortedProducts = async (req, res) => {
+    const myData = await Products.find(req.query).sort("-price");
+    //Sort price asc ("price") - Sort price desc ("-price")
+    res.status(200).json({ myData });
+};
+
+const getSortedProductsWithFilters = async (req, res) => {
+    const { company, name, sort } = req.query;
+    const queryObj = {};
+    if (company) {
+        queryObj.company = company;
+    }
+    if (name) {
+        queryObj.name = { $regex: name, $options: "i" };
+        //Full text search functionality - $regex with $options
+    }
+
+    let apiData = Products.find(queryObj);
+    if (sort) {
+        let sortify = sort.replace(",", " ");
+        apiData = apiData.sort(sortify);
+    }
+
+    const myData = await apiData;
+    res.status(200).json({ myData });
+};
+//For test: /api/products/sf?company=Bach%20Hoa%20Xanh&sort=-price
+
+const getSelectedProducts = async (req, res) => {
+    const { company, name, sort, select } = req.query;
+    const queryObj = {};
+    if (company) {
+        queryObj.company = company;
+    }
+    if (name) {
+        queryObj.name = { $regex: name, $options: "i" };
+        //Full text search functionality - $regex with $options
+    }
+
+    let apiData = Products.find(queryObj);
+    if (sort) {
+        let sortify = sort.replace(",", " ");
+        apiData = apiData.sort(sortify);
+    }
+
+    if (select) {
+        let selectify = select.split(",").join(" ");
+        apiData = apiData.select(selectify);
+    }
+
+    const myData = await apiData;
+    res.status(200).json({ myData });
+};
+
+const pagination = async (req, res) => {
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+    let skip = (page - 1) * limit;
+    let apiData = Products.find({}).skip(skip).limit(limit);
+
+    const myData = await apiData;
+    res.status(200).json({ myData, nbHits: myData.length });
+};
+
+module.exports = { pagination, getAllProducts, getAllProductsTest, getQueryProducts, getQueryProductsWithCompanyFilter, getQueryProductsWithFilters, getSortedProducts, getSortedProductsWithFilters, getSelectedProducts };
--- REST/routes/products.js
+++ REST/routes/products.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getAllProducts, getAllProductsTest } = require("../controllers/products");
+const { pagination, getAllProducts, getAllProductsTest, getQueryProducts, getQueryProductsWithCompanyFilter, getQueryProductsWithFilters, getSortedProducts, getSortedProductsWithFilters, getSelectedProducts } = require("../controllers/products");
 
 router.route("/").get(getAllProducts);
 router.route("/test").get(getAllProductsTest);
+router.route("/query").get(getQueryProducts);
+router.route("/company").get(getQueryProductsWithCompanyFilter);
+router.route("/filter").get(getQueryProductsWithFilters);
+router.route("/sort").get(getSortedProducts);
+router.route("/sf").get(getSortedProductsWithFilters);
+router.route("/select").get(getSelectedProducts);
+router.route("/pag").get(pagination);
 
 module.exports = router;
